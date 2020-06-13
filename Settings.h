baf6e96fa2f5899cbd899ca01e146d9849dae805
Sergey Vanichkin <sergey@vanichkin.ru>
2020-06-13 18:55:37 +0300

    v. 2.1.1
    Add Apache License

--- Settings.h
+++ Settings.h
@@ -5,6 +5,19 @@
 //  Created by Сергей Ваничкин on 19.08.16.
 //  Copyright © 2016 👽 Technology. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//
 //  Пример использования
 //
 //  [Settings.application setObject:@"Test" forKey:@"TestKey"];
